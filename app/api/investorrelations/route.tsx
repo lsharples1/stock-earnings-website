86266c8d7ad4f850d6b0e9e2028998cb9eaadeae
Lilly Sharples <62436961+lsharples1@users.noreply.github.com>
2024-04-27 14:02:28 -0700

    get quarterly results page for a ticker

--- app/api/investorrelations/route.tsx
+++ app/api/investorrelations/route.tsx
@@ -1,5 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
 import yahooFinance from 'yahoo-finance2';
+import puppeteer from "puppeteer";
+import { JSDOM } from 'jsdom';
+import OpenAI from 'openai';
+import { getJson } from 'serpapi';
 
 /**
  * 
@@ -11,53 +15,155 @@ export async function POST(
 ) {
     const body = await request.json();
     console.log('body', body);
-    const assetProfile = await yahooFinance.quoteSummary(body.ticker, {
+    const documentResponses = await getDocumentResponses(body);
+    console.log('documentResponses', documentResponses);
+   
+    return NextResponse.json({ documentResponses: [{ documentType: body.documentType[0], fiscalPeriod: body.fiscalPeriod[0], documentResponse: documentResponses[0] }] });
+}
+
+
+async function getDocumentResponses(body: { ticker: string; documentType: string[]; fiscalPeriod: string[]; }) {
+     // 1. get the investor relations url for the company
+     const investorRelationsPageUrl = await getInvestorRelationsUrl(body.ticker);
+    console.log('investorRelationsPageUrl', investorRelationsPageUrl);
+
+     // 2. using the investor relations website, get the quarterly results page url
+     const quarteryResultsPageUrl = await getQuarterlyResultsPageUrl(investorRelationsPageUrl);
+     console.log('quarteryResultsPageUrl', quarteryResultsPageUrl);
+ 
+    //  const documentResponses = await findDocumentsFromIRWebsite(irWebsite, body.documentType, body.fiscalPeriod);
+    //  console.log('documentResponses', documentResponses);
+     
+     // response format: [{ documentType: string; fiscalPeriod: string; documentResponse: string; }[]]
+     return [investorRelationsPageUrl];
+
+}
+
+
+async function getInvestorRelationsUrl(ticker: string) {
+    const assetProfile = await yahooFinance.quoteSummary(ticker, {
         modules: ['assetProfile']
     
     });
     let irWebsite;
     if (assetProfile.assetProfile?.irWebsite) {
         // if we have the IR website, great now we can start the next step of getting earnings reports
-        irWebsite = assetProfile.assetProfile.irWebsite;
-        return NextResponse.json({ documentResponses: [{ documentType: body.documentType[0], fiscalPeriod: body.fiscalPeriod[0], documentResponse: irWebsite }] });
-
+        return assetProfile.assetProfile.irWebsite;
     } else {
-        // if Yahoo Finance doesn't have a generic website, throw an error for now
-        if (!assetProfile.assetProfile?.website) {
-            return NextResponse.json({ irWebsite: null });
+        if (assetProfile.assetProfile?.website) { // if we have the company's home page, we will use that to find the IR website
+            irWebsite = await getInvestorRelationsUrlUtil(assetProfile.assetProfile.website.replace('https://www.', ''));
+
+        } else if (assetProfile.assetProfile?.name) { // if we have the company's name, we will use that to find the IR website
+            irWebsite = await getInvestorRelationsUrlUtil(assetProfile.assetProfile.name);
         }
-        // sometimes the website is the IR website so we need to check if "invest" is in the URL, then we can assume it's the IR website
-        if (assetProfile.assetProfile?.website?.includes('invest')) {
-            irWebsite = assetProfile.assetProfile.website;
-            // if we have the IR website, great now we can start the next step of getting earnings reports
-            return NextResponse.json({ documentResponses: [{ documentType: body.documentType[0], fiscalPeriod: body.fiscalPeriod[0], documentResponse: irWebsite }] });
-        }
-        // if we don't have the IR website, we can get the company's website, and then we will use a utility to find the IR website
-        irWebsite = await findIRWebsite(assetProfile.assetProfile.website.replace('https://www.', ''));
-        return NextResponse.json({ documentResponses: [{ documentType: body.documentType[0], fiscalPeriod: body.fiscalPeriod[0], documentResponse: irWebsite }] });
+        return irWebsite;
     }
 }
 
-async function findIRWebsite(website: string) {
-    // we will use a utility to find the IR website based on the company's website
-    // starting with a heuristic approach... investors.website, investor.website, website/ir, website/investors, 
-    // we'll go through these, trying a get request to see if it's a valid page, if it is, we'll return it
-    // if none of these work, we'll return the original website
+async function getInvestorRelationsUrlUtil(ticker: string) {
+    const serpResponse = await getJson({
+        q: `${ticker} investor relations site`,
+        location: 'United States',
+        hl: 'en',
+        gl: 'us',
+        google_domain: 'google.com',
+        api_key: process.env.SERP_API_KEY,
+    });
+    console.log('serpResponse', serpResponse.organic_results);
+    return serpResponse.organic_results[0].link;
+}
 
-    // TODO: maybe use LLM here to genereate the heuristics
-    const heuristics = [`https://investors.${website}`, `https://investor.${website}`, `https://ir.${website}`, `https://${website}/ir`, `https://${website}/investors`];
+async function getQuarterlyResultsPageUrl(irWebsite: string) {
+    // first scrape the IR home page to find the link to the quarterly results page
+    const links = await scrapeHomePageForLinks(irWebsite);
 
-    console.log('heuristics', heuristics);
-    for (const heuristic of heuristics) {
-        try {
-            const response = await fetch(heuristic, {redirect: 'follow'});
-            console.log('response', response);
-            if (response.ok) {
-                console.log('found IR website', heuristic);
-                return heuristic;
-            }
-        } catch (e) {
-            console.error(e);
+    // create a propmt asking OpenAI to interpret the links to find the quarterly results page
+    const prompt = `Interpret the links to find the quarterly earnings results page for the company. Please return the link in this json format: {earningsPage: urlToEarningsPage} The links are: \n\n${links.map(link => link.href).join('\n')}`;
+    const response = await openAiChatResponse(prompt);
+    return response.earningsPage;
+}
+
+
+
+/**
+ * 
+ * @param irWebsite the investor relations website for the company
+ * @param documentType  containing any of [EarningsRelease, EarningsPresentation, EarningsWebcast]
+ * @param fiscalPeriod containing 1+ fiscal periods to return documents for
+ * @returns documentResponses: [{ documentType: string; fiscalPeriod: string; documentResponse: string; }[]]
+ */
+// async function findDocumentsFromIRWebsite(irWebsite: string, documentType: string[], fiscalPeriod: string[]) {
+//     // starting with just 1Q2024 and documentType EarningsRelease, will expand to other document types and fiscal periods later
+
+//     // first we need to scrape the IR website to find the links to the documents
+//     const links = await scrapeHomePageForLinks(irWebsite);
+//     console.log('links', links);
+
+//     const relevantLinks = await openAiInterpretLinks(links, documentType, fiscalPeriod);
+//     console.log('relevantLinks', relevantLinks);
+// }
+
+
+
+
+async function scrapeHomePageForLinks(irWebsite: string) {
+    const data = await fetch(irWebsite);
+    const html = await data.text();
+
+    // get the main content of the page
+    const { document } = (new JSDOM(html)).window;
+
+    // Get all <a> tags from the document
+    const links = document.querySelectorAll('a');
+    const linkDetails = Array.from(links).map(link => ({
+        href: link.href,  // Get the href attribute
+        text: link.textContent.trim()  // Get the text content, trimmed
+    }));
+
+    return linkDetails;  // You might want to return this if needed elsewhere
+}
+
+/**
+ * 
+ * @param prompt the prompt to send to OpenAI
+ * @returns the response from OpenAI
+ */
+async function openAiChatResponse(prompt: string) {
+    const openai = new OpenAI({
+        apiKey: process.env.OPENAI_API_KEY!,
+      });
+      const response = await openai.chat.completions.create({
+        model: 'gpt-4',
+        messages: [
+          {
+            role: 'system',
+            content: prompt,
+          },
+        ],
+      })
+        console.log('response', response);
+        const message = response.choices[0].message;
+        console.log('message', message);
+        if (message.content) {
+            return JSON.parse(message.content)
+        } else {
+            console.error('Error with OpenAI response', message);
+            throw new Error('Error with OpenAI response');
         }
+}
+
+async function scrapeHomePagePup(irWebsite: string) {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(irWebsite, { waitUntil: 'networkidle0' }); // Ensures all scripts are fully loaded
+
+    const anchors = await page.evaluate(() => {
+        const links = Array.from(document.querySelectorAll('a'));
+        return links.map(link => link.href);
     }
+    );
+    console.log('anchors', anchors);
+
+    await browser.close();
+    return anchors;
 }
